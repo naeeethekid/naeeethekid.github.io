b99f9bf2e53ac52328e30e7818b042522e426714
naeeethekid <nessaalissonestime@gmail.com>
2018-07-05 10:28:55 -0400

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+function nightMode(){
+  // document.getElementById("night").src="tumblr.gif"
+
+  document.getElementById('bg').style.background = "url('tumblr.gif')"
+}
+
+function dayMode(){
+
+  document.getElementById('bg').style.background = "url('tumblr.png')"
+}
